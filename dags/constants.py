832246a8a3c7b1df43832c44046172c0a1f60dac
HieuFromWaterloo <ng.hieu100@gmail.com>
2023-11-06 20:57:08 -0500

    updated constants and main dag

--- dags/constants.py
+++ dags/constants.py
@@ -1,1 +1,14 @@
-import os
+# import os 
+from datetime import datetime, timedelta
+# from dotenv import load_dotenv
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 07, 11),
+    'email': [],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
--- dags/main_dag.py
+++ dags/main_dag.py
@@ -5,23 +5,20 @@ import json
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
 import pandas as pd
+from constants import *
 
-
-
-
-def kelvin_to_fahrenheit(temp_in_kelvin):
-    temp_in_fahrenheit = (temp_in_kelvin - 273.15) * (9/5) + 32
-    return temp_in_fahrenheit
+def kelvin_to_celcius(temp_in_kelvin):
+    return (temp_in_kelvin - 273.15)
 
 
 def transform_load_data(task_instance):
     data = task_instance.xcom_pull(task_ids="extract_weather_data")
     city = data["name"]
     weather_description = data["weather"][0]['description']
-    temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp"])
-    feels_like_farenheit= kelvin_to_fahrenheit(data["main"]["feels_like"])
-    min_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_min"])
-    max_temp_farenheit = kelvin_to_fahrenheit(data["main"]["temp_max"])
+    temp_farenheit = kelvin_to_celcius(data["main"]["temp"])
+    feels_like_farenheit= kelvin_to_celcius(data["main"]["feels_like"])
+    min_temp_farenheit = kelvin_to_celcius(data["main"]["temp_min"])
+    max_temp_farenheit = kelvin_to_celcius(data["main"]["temp_max"])
     pressure = data["main"]["pressure"]
     humidity = data["main"]["humidity"]
     wind_speed = data["wind"]["speed"]
@@ -48,22 +45,8 @@ def transform_load_data(task_instance):
 
     now = datetime.now()
     dt_string = now.strftime("%d%m%Y%H%M%S")
-    dt_string = 'current_weather_data_portland_' + dt_string
+    dt_string = f'current_weather_data_toronto_{dt_string}'
     df_data.to_csv(f"s3://weatherapiairflowyoutubebucket-yml/{dt_string}.csv", index=False, storage_options=aws_credentials)
-
-
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 1, 8),
-    'email': ['myemail@domain.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 2,
-    'retry_delay': timedelta(minutes=2)
-}
-
 
 
 with DAG('weather_dag',
@@ -73,27 +56,24 @@ with DAG('weather_dag',
 
 
         is_weather_api_ready = HttpSensor(
-        task_id ='is_weather_api_ready',
-        http_conn_id='weathermap_api',
-        endpoint='/data/2.5/weather?q=Portland&APPID=5031cde3d1a8b9469fd47e998d7aef79'
+            task_id ='is_weather_api_ready',
+            http_conn_id='weathermap_api',
+            endpoint='/data/2.5/weather?q=Toronto&APPID=5031cde3d1a8b9469fd47e998d7aef79'
         )
 
 
         extract_weather_data = SimpleHttpOperator(
-        task_id = 'extract_weather_data',
-        http_conn_id = 'weathermap_api',
-        endpoint='/data/2.5/weather?q=Portland&APPID=5031cde3d1a8b9469fd47e998d7aef79',
-        method = 'GET',
-        response_filter= lambda r: json.loads(r.text),
-        log_response=True
+            task_id = 'extract_weather_data',
+            http_conn_id = 'weathermap_api',
+            endpoint='/data/2.5/weather?q=Toronto&APPID=5031cde3d1a8b9469fd47e998d7aef79',
+            method = 'GET',
+            response_filter= lambda r: json.loads(r.text),
+            log_response=True
         )
 
         transform_load_weather_data = PythonOperator(
-        task_id= 'transform_load_weather_data',
-        python_callable=transform_load_data
+            task_id= 'transform_load_weather_data',
+            python_callable=transform_load_data
         )
-
-
-
 
         is_weather_api_ready >> extract_weather_data >> transform_load_weather_data
