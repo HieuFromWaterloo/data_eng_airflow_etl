2c9d9b6fb9cf1cdc2f4bf5dfae7ff04cc761ff98
HieuFromWaterloo <ng.hieu100@gmail.com>
2023-11-09 16:23:05 -0500

    added redshift table query

---
+++ redshift_query.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS TorontoHousing (
+    bathrooms NUMERIC,
+    bedrooms NUMERIC,
+    city VARCHAR(128),
+    homeStatus VARCHAR(128),
+    homeType VARCHAR(128),
+    livingArea NUMERIC,
+    price NUMERIC,
+    rentZestimate NUMERIC,
+    zipcode VARCHAR(16)
+)
